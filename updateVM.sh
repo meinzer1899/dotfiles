85a356300abcf3b219d143ef258ef44fec692e6a
meinzer1899 <44314956+meinzer1899@users.noreply.github.com>
2022-02-01 16:43:03 +0100

    Update all cargo packages when updating VM.

--- updateVM.sh
+++ updateVM.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
 tldr --update
-zinit self-update
-zinit update --parallel
+
+# zinit update --parallel && zinit self-update
+
 npm install -g npm
+
 ~/.tmux/plugins/tpm/bin/update_plugins all
 ~/.tmux/plugins/tpm/bin/clean_plugins
+
+# cargo install cargo-update is needed
+cargo install-update -a
