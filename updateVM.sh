fa7ff81c293e3c283cdf1191e00a991f740c6842
meinzer1899 <44314956+meinzer1899@users.noreply.github.com>
2022-07-28 11:16:49 +0200

    Add pip in update script.

--- updateVM.sh
+++ updateVM.sh
@@ -13,3 +13,7 @@ tldr --update
 
 # cargo install cargo-update is needed
 cargo install-update -a
+
+# pip
+pip install -U pip
+pip freeze --local | tee before_upgrade.txt | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
