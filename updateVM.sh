7c641a8881e032c2184f43b64e7e66c557e7f48c
meinzer1899 <44314956+meinzer1899@users.noreply.github.com>
2022-11-11 17:57:18 +0100

    Update VM: Remove fzf (is installed and updated via zi).

--- updateVM.sh
+++ updateVM.sh
@@ -4,7 +4,8 @@ tldr --update
 
 zi update --all --parallel
 
-nvm install 'lts/*' --reinstall-packages-from=default --latest-npm
+# nvm install 'lts/*' --reinstall-packages-from=default --latest-npm
+nvm --install-latest-npm
 
 ~/.tmux/plugins/tpm/bin/update_plugins all
 ~/.tmux/plugins/tpm/bin/clean_plugins
@@ -15,6 +16,3 @@ cargo install-update -a
 # pip
 pip install -U pip
 pip freeze --local | tee before_upgrade.txt | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
-
-#fzf
-cd ~/.fzf && git pull && ./install
