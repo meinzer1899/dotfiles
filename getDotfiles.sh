3d28ed882f4346fa795c0f9c72a8c36a0185d4e9
meinzer1899 <44314956+meinzer1899@users.noreply.github.com>
2022-11-11 17:53:49 +0100

    Add config files for bat and tealdeer.

--- getDotfiles.sh
+++ getDotfiles.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+mkdir -p .config/tealdeer
+mkdir -p .config/bat
+
 cp ~/.zshrc .
 cp ~/.vimrc .
 cp ~/.tmux.conf .
 cp ~/.vim/coc-settings.json .
 cp ~/.alacritty.yml .
+cp ~/.config/tealdeer/config.toml .config/tealdeer/
+cp ~/.config/bat/config .config/bat/
