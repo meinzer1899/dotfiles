09cf3a417bef0fb590f6852b161f21568464d812
Sebastian Meyer <sebastian.meyer@itk-engineering.de>
2023-09-08 16:06:55 +0200

    Improve updateVM.sh.

--- updateVM.sh
+++ updateVM.sh
@@ -3,11 +3,17 @@
 tldr --update
 
 # https://wiki.zshell.dev/docs/guides/commands
-zi update --all --parallel --quiet
+zi update --all --parallel --quiet && zi self-update
 
-# installs latest LTS with volta
-volta install node
-# from time to time, uninstall old node versions via rm -rf in ~/.volta/tools/image/node/<version>
+# ripgrep rg, fd and bat
+# because of bug (error command rg ...) in fzf.vim (or vim shellescape) when calling Rg or RG
+sudo cp -t /usr/local/bin ~/.zi/polaris/bin/{bat,fd,rg}
+
+# javascript tool manager, installs node.js runtimes, npm and Yarn package managers or any binary from npm
+volta install node # installs latest LTS
+volta install bash-language-server
+volta install npm # do I need npm or does volta do the job?
+# from time to time, uninstall old package versions via rm -rf in ~/.volta/tools/image/[node|npm]
 
 ~/.tmux/plugins/tpm/bin/update_plugins all
 ~/.tmux/plugins/tpm/bin/clean_plugins
@@ -20,17 +26,14 @@ pip install -U pip
 pip freeze --local | tee before_upgrade.txt | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 
 # vim
-vim +PlugUpdate +CocUpdate +PlugClean! +qall
+vim +PlugUpdate +CocUpdateSync +PlugClean! +qall
 
 # tmux
-# manual via repository
-#
-# ripgrep rg, fd and bat
-# because of bug (error command rg ...) in fzf.vim (or vim shellescape) when calling Rg or RG
-#
-sudo cp ~/.zi/plugins/sharkdp---bat/bat/bat ~/.zi/plugins/sharkdp---fd/fd/fd ~/.zi/plugins/BurntSushi---ripgrep/target/release/rg /usr/local/bin
-#
-#
+# manually via repository
+
+# alacritty
+# manually via repository
+
 # hadolint
 # https://github.com/hadolint/hadolint
-# tip: download release binary, cp to /usr/bin
+# TODO: install via zi
