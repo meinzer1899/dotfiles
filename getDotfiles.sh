e1bb31f02775132cd22744fc7d4bb541bca98c32
Sebastian Meyer <sebastian.meyer@itk-engineering.de>
2023-10-23 10:24:48 +0200

    Add ruff.toml.

--- getDotfiles.sh
+++ getDotfiles.sh
@@ -3,10 +3,12 @@
 mkdir -p .config/tealdeer
 mkdir -p .config/bat
 mkdir -p .config/clangd
+mkdir -p .config/ruff
 
 cp ~/.config/tealdeer/config.toml .config/tealdeer/
 cp ~/.config/bat/config .config/bat/
 cp ~/.config/clangd/config.yaml .config/clangd/
+cp ~/.config/ruff/.ruff.toml .config/ruff/
 
 cp -t . \
     ~/.zshrc \
