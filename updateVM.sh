f1e8acdb83e65dc63fbac9ce89e944ade998d190
Sebastian Meyer <sebastian.meyer@itk-engineering.de>
2023-12-19 18:15:05 +0100

    Improve readability of updateVM.sh.

--- updateVM.sh
+++ updateVM.sh
@@ -1,29 +1,41 @@
 #!/bin/env bash
 
+# enable bash strict mode
+# (http://redsymbol.net/articles/unofficial-bash-strict-mode/)
+set -euo pipefail
+IFS=$'\n\t'
+
+echo "Starting update"
 tldr --update
 
 # https://wiki.zshell.dev/docs/guides/commands
-zi update --all --parallel --quiet && zi self-update
+# zi update --all --parallel --quiet && zi self-update
+# zsh -lic "zi update --all --quiet"
 
-# ripgrep rg, fd and bat
-# because of bug (error command rg ...) in fzf.vim (or vim shellescape) when calling Rg or RG
-sudo cp -t /usr/local/bin ~/.zi/polaris/bin/{bat,fd,rg}
+# rg, fd and bat
+# because of bug (error command rg ...) in fzf.vim (or vim shellescape) when calling Rg or RG,
+# I need the executables also in /usr/local/bin (symlink not working)
+cp -vt ~/.zi/polaris/bin ~/.zi/plugins/sharkdp---fd/fd/fd ~/.zi/plugins/sharkdp---bat/bat/bat
+sudo cp -vt /usr/local/bin ~/.zi/polaris/bin/{bat,fd}
 
 # javascript tool manager, installs node.js runtimes, npm and Yarn package managers or any binary from npm
-volta install node # installs latest LTS; node@20 installs latest node 20 version
+volta install node@lts # installs latest LTS; node@20 installs latest node 20 version
 volta install bash-language-server
-volta install npm # needed for coc plugin install
+# https://docs.npmjs.com/about-npm-versions recommends to install latest version
+volta install npm@latest # needed for coc plugin install
 # from time to time, uninstall old package versions via rm -rf in ~/.volta/tools/image/[node|npm]
 
-~/.tmux/plugins/tpm/bin/update_plugins all
+# fails when called out of tmux session
 ~/.tmux/plugins/tpm/bin/clean_plugins
+~/.tmux/plugins/tpm/bin/update_plugins all
 
 # cargo install cargo-update is needed
-cargo install-update -a
+# cargo install-update -a
 
+# gets updated via zi
 # pip
-pip install -U pip
-pip freeze --local | tee before_upgrade.txt | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+pip install --upgrade pip
+# pip freeze --local | tee before_upgrade.txt | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
 
 # vim
 vim +PlugUpdate +CocUpdateSync +PlugClean! +qall
@@ -33,3 +45,5 @@ vim +PlugUpdate +CocUpdateSync +PlugClean! +qall
 
 # alacritty
 # manually via repository
+echo "Updating done"
+exit 0
