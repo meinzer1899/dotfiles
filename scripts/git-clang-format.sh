dccf91c833efc104389875cc32608ff386a969e7
meinzer1899 <44314956+meinzer1899@users.noreply.github.com>
2024-07-03 16:22:50 +0200

    git-clang-format.sh: Improve by removing for loop (and using variable expansion instead) and by removing checking if file exists locally by using ``git merge-base``.

--- scripts/git-clang-format.sh
+++ scripts/git-clang-format.sh
@@ -1,16 +1,25 @@
 #!/usr/bin/env bash
 
 # @brief  Checks sources formatting with clang-format on files that differ from $CI_DEFAULT_BRANCH (defaults to main if not set).
-#
+
 # @details  Files and directories are excluded when the caller sets a (extended) regex pattern EXCLUDE_PATTERN.
 
+# Documentation for set and shopt: https://www.gnu.org/software/bash/manual/html_node/Modifying-Shell-Behavior.html
+# no -o errexit, because grep returns error code if no match was found
 set -o pipefail -o nounset
-# no -o errexit, because we want clang-format to check all files first independent of error code
 IFS=$'\n\t'
+
+print_message_and_exit() {
+    echo "$1"
+    echo "Exiting."
+    exit 0
+}
 
 files()
 {
-    git diff origin/"${CI_DEFAULT_BRANCH:="main"}" --name-only | grep -E '.*\.(c|cpp|h|hpp|tpp)\b';
+    # using merge-base instead of plain origin/main to get rid of check if file is present locally which is
+    # not the case if file was added only to remote but is not present in HEAD
+    git diff "$(git merge-base origin/"${CI_DEFAULT_BRANCH:="main"}" HEAD)" --name-only | grep -E '.*\.([cht]pp|[ch])\b';
 }
 
 if [[ -n ${EXCLUDE_PATTERN:=""} ]]; then
@@ -19,19 +28,24 @@ else
     exclude() { cat; }
 fi;
 
-files=$(files | exclude)
+# grep exits with error code 1 if no match is found
+files_to_check=$(files | exclude) || print_message_and_exit "No files found to format."
+# shellcheck disable=SC2086
+# I want word splitting here
+clang-format --dry-run --ferror-limit=0 --Werror $files_to_check
 
-ret=0
-for FILE in $files
-do
-    if [[ ! -e $FILE ]]; then
-        # if file exists on target ($CI_DEFAULT_BRANCH) but not in source (local) branch, clang-format emits "No such file or directory"
-        #TODO: investigate if this condition can be removed by using
-        # git diff $(git merge-base origin/main HEAD) in function files()
-        continue
-    fi
-    # we set error code but don't care about clang-format error code (which is not documented in man page anyway)
-    clang-format --dry-run --ferror-limit=0 --Werror "$FILE" || ret=1
-done
+# version using for loop instead of variable expansion via IFS and condition check if file exists locally
 
-exit $ret
+# no -o errexit, because clang-format exits we want clang-format to check all files first independent of error code
+#ret=0
+#for FILE in $files
+#do
+#    if [[ ! -e $FILE ]]; then
+#        # if file exists on target ($CI_DEFAULT_BRANCH) but not in source (local) branch, clang-format emits "No such file or directory"
+#        continue
+#    fi
+#    # we set error code but don't care about clang-format error code (which is not documented in man page anyway)
+#    clang-format --dry-run --ferror-limit=0 --Werror "$FILE" || ret=1
+#done
+
+#exit $ret
