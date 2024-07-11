b506f53afeece3a4e856fe5176ad6a6b13843e71
meinzer1899 <44314956+meinzer1899@users.noreply.github.com>
2024-07-11 11:02:42 +0200

    update.sh: Install vint directly from github repository instead of using --pre.

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,7 @@
 
 # enable bash strict mode
 # (http://redsymbol.net/articles/unofficial-bash-strict-mode/)
-set -eo pipefail
+set -o pipefail -o nounset
 IFS=$'\n\t'
 
 echo "Starting update"
@@ -31,8 +31,7 @@ fi
 # gets updated via zi
 # pip (https://www.mankier.com/1/pip#Commands)
 python3 -m pip install --upgrade pip
-python3 -m pip install --upgrade --pre vim-vint # has to be pre (https://github.com/iamcco/coc-diagnostic/issues/62#issuecomment-735660297)
-# pip freeze --local | tee before_upgrade.txt | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+python3 -m pip install git+https://github.com/Vimjas/vint.git  #has to be pre (https://github.com/iamcco/coc-diagnostic/issues/62#issuecomment-735660297)
 
 # vim
 vim +PlugUpdate +CocUpdateSync +PlugClean! +qall
